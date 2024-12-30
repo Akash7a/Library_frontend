fdd03785463c7aee3177a34f475c100448ef5a90
Akash Kumar <akashkumar914a@gmail.com>
2024-12-30 15:25:50 +0530

    changed the url of frontend

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:3000";
+
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api': "http://localhost:3000"
+      '/api': apiUrl, // Proxy API requests to backend server
     }
   },
   plugins: [react()],
